049de87971224ca7ddd35c602fe9abe447eed26c
Mohamed Kandil <medokandeal7@gmail.com>
2026-08-05 15:27:33 +0200

    refactor: move app/src/test/java/com/egyptian/agent/executor/CallExecutorTest.java to android/src/test/java/com/egyptian/agent/executor/CallExecutorTest.java

--- android/src/test/java/com/egyptian/agent/executor/CallExecutorTest.java
+++ android/src/test/java/com/egyptian/agent/executor/CallExecutorTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import com.egyptian.agent.nlu.IntentResult;
+import com.egyptian.agent.nlu.IntentType;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
