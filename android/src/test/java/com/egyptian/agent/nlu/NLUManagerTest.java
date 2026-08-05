566c10f5e82d35403dc1a2aafbbbf7ce8d235633
Mohamed Kandil <medokandeal7@gmail.com>
2026-08-05 15:27:35 +0200

    refactor: move app/src/test/java/com/egyptian/agent/nlu/NLUManagerTest.java to android/src/test/java/com/egyptian/agent/nlu/NLUManagerTest.java

--- android/src/test/java/com/egyptian/agent/nlu/NLUManagerTest.java
+++ android/src/test/java/com/egyptian/agent/nlu/NLUManagerTest.java
@@ -3,6 +3,7 @@ package com.egyptian.agent.nlu;
 import android.content.Context;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
